bf04d39afb3f5874bc30940a6a000457a406e826
MI CYBER WARRIOR <ibad0469427@gmail.com>
2024-12-12 11:27:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,71 @@
+// Function to clear screen based on OS
+function clearScreen() {
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        system('cls');
+    } else {
+        system('clear');
+    }
+}
+
+// Function to generate random user agent
+function generateUserAgent() {
+    $os = ['Windows', 'Linux', 'iOS', 'Android'];
+    $versions = ['8', '9', '10', '11', '12', '13', '14'];
+    $devices = ['Samsung', 'Motorola', 'Xiaomi', 'Huawei', 'OnePlus'];
+    
+    $selectedOs = $os[array_rand($os)];
+    
+    if ($selectedOs === 'Android') {
+        $version = $versions[array_rand($versions)];
+        $device = $devices[array_rand($devices)];
+        $userAgent = "Mozilla/5.0 (Linux; Android $version; $device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36";
+    } else {
+        $userAgent = "Mozilla/5.0 ($selectedOs NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36";
+    }
+    
+    return $userAgent . rand(1000000, 9999999);
+}
+
+// Function to print colored text
+function printColored($text, $color) {
+    return "\033[" . $color . "m" . $text . "\033[0m";
+}
+
+// Color codes
+$green = "32";
+$red = "31"; 
+$yellow = "33";
+$blue = "34";
+
+// Function to print banner
+function printBanner() {
+    global $green;
+    $banner = "
+-------------------------------------------------
+██ ███████ ███████ ████████
+██ ██   ██ ██   ██ ██    ██
+██ ███████ ███████ ██    ██
+██ ██   ██ ██   ██ ██    ██
+██ ███████ ██   ██ ████████
+-------------------------------------------------
+
+     - Clayton Script-
+     
+              - VERSION 2.0 -
+    
+- MADE BY : Ibad (MI CYBER WARRIOR)
+- Telegram: @MICYBERWARRIOR 
+- channel: https://t.me/MICYBERWARRIOR 
+
+- Note: If you encounter the issue \"URL not found\"
+  kindly ignore it.  
+- PX Points will be added to your account within 20 seconds.
+
+-------------------------------------------------
+
+";
+    echo printColored($banner, $green);
+}
 from aiohttp import (
     ClientResponseError,
     ClientSession,
